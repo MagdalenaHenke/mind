0cd860e51c6f52070e1689282d645e1b4b9c08b2
lena <lena@floored.com>
2019-12-17 19:37:27 -0500

    add prettier and run on index.js

--- index.js
+++ index.js
@@ -3,28 +3,28 @@ var http = require('http').createServer(app);
 var path = require('path');
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-
 // socket.io related things
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
   console.log('a user connected');
-  socket.on('disconnect', function(){
+  socket.on('disconnect', function() {
     console.log('user disconnected');
   });
 
-  socket.on('number pick', function(msg){
+  socket.on('number pick', function(msg) {
     console.log('message: ' + msg);
+  });
+
+  socket.on('number pick', function(msg) {
+    io.emit('chat message', msg);
   });
 });
 
-
 // end of socket.io related things
 
-
-
-http.listen(4422, function(){
+http.listen(4422, function() {
   console.log('listening on *:4422');
 });
